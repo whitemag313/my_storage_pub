10a79175d960e63752cd24718ff17e6ef89b90c7
Michael Timchenko <96528293+whitemag313@users.noreply.github.com>
2022-10-12 18:20:25 +0300

    Update prep_windows.bat

--- Windows/prep_windows.bat
+++ Windows/prep_windows.bat
@@ -7,7 +7,7 @@ net accounts /minpwlen:12
 # disable telemetry
 sc config "diagtrack" start=disabled
 sc config "dmwappushservice" start=disable
-reg add "HKLM\SOFTWARE\Policies\\Microsoft\Windows\DataCollection"/v AllowTelemtery/t REG_DWORD/d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection"/v AllowTelemtery/t REG_DWORD/d 0 /f
 
 # delete onedrive (version 64)
 TASKKILL /f /im OneDrive.exe
@@ -32,5 +32,5 @@ sc config "termservice" start=disabled
 reg delete "HKEY_CLASSES_ROOT\ms-msdt" /va /f
 
 # enableAuthenticodeVerification64
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Cryptography\Wintrust\Config"/v EnableCertPaddingCheck/t REG_DWORD/d 1 /f
-reg add "HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Cryptography\Wintrust\Config"/v EnableCertPaddingCheck/t REG_DWORD/d 1 /f
+reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Cryptography\Wintrust\Config /v EnableCertPaddingCheck/t REG_DWORD/d 1 /f
+reg add HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Cryptography\Wintrust\Config /v EnableCertPaddingCheck/t REG_DWORD/d 1 /f
